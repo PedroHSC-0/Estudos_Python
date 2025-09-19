46c004cf6460730f2901e4eac681ba3e5c96acbd
Pedro Henrique Silva Costa <pedritohscostaph67@gmail.com>
2025-09-18 23:42:30 -0300

    .

--- Python/Trabalhos/Campo Minado/Campo_Minado.py
+++ Python/Trabalhos/Campo Minado/Campo_Minado.py
@@ -5,7 +5,6 @@ import time
 # Função para avisar erro personalizado
 def aviso_erro(mensagem, atraso):
     os.system('cls')
-    time.sleep(0.5)
     print(mensagem, end="")
     for i in range(3):
         time.sleep(atraso)
@@ -14,13 +13,35 @@ def aviso_erro(mensagem, atraso):
     time.sleep(atraso)
     os.system('cls')
 
+def perdeu():
+    os.system('cls')
+    print(r"|--------------------------------------|")
+    print(r"| |¨¨¨¨\    /¨¨¨¨\  /¨¨¨¨\  |\      /| |")
+    print(r"| |     )  |      ||      | | \    / | |")
+    print(r"| |¨¨¨¨¨)  |      ||      | |  \  /  | |")
+    print(r"| |____/    \____/  \____/  |   \/   | |")
+    print(r"|--------------------------------------|")
+    print("\n")
+    print("PERDEU")
+    
+    for i in range(40):
+        time.sleep(0.1)
+        print(".", end="", flush=True)
+
+    os.system('cls')
+
 # Função de entrada de dados(Tamanho do campo e número de minas)
 def input_dados():
     while(True):
         try:
-            tamanho = int(input("Insira o tamanho do campo minado: "))
+            tamanho = int(input("Insira o tamanho do campo minado: (Max: 10) "))
+            
+            if(tamanho > 10):
+                aviso_erro("Insira um tamanho menor ou igual a 10", 0.5)
+                continue
+
             while(True):
-                try: 
+                try:
                     n_minas = int(input("Insira o número de minas contidas no campo: "))
                     break
                 
@@ -52,3 +73,4 @@ tamanho, n_minas = input_dados()
 campo  = gerar_campo(tamanho, n_minas)
 
 print(campo)
+perdeu()
